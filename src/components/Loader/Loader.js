541535c4b68ce107462909b9133f96928e75f415
Harman Pannu <iharmanpannu@gmail.com>
2018-03-08 20:23:11 -0800

    completed design

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../Loader/Loader.css';
+
+class Loader extends Component {
+  render() {
+    return (
+      <div class="container">
+        <ul>
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Loader;
--- src/util/Fetch.js
+++ src/util/Fetch.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+
+import Loader from '../components/Loader/Loader';
+
 import '../util/Fetch.css';
 export class Fetch extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      users: [],
+      isLoading: true
     };
   }
 
@@ -13,14 +17,15 @@ export class Fetch extends Component {
   }
 
   fetchData() {
-    const url = 'https://randomuser.me/api/?results=10&nat=us,uk,fr,nz';
+    const url = 'https://randomuser.me/api/?results=200&nat=usa,uk,nz';
     fetch(url)
       .then(response => {
         return response.json();
       })
       .then(parsedJSON => {
         this.setState({
-          users: parsedJSON.results
+          users: parsedJSON.results,
+          isLoading: false
         });
       })
       .catch(error => console.log('parsing failed', error));
@@ -31,93 +36,88 @@ export class Fetch extends Component {
 
     return (
       <div>
-        <div />
-        <div className="table">
-          <div className="table-header">
-            <div className="header__item">
-              <a>First Name</a>
-            </div>
-            <div className="header__item">
-              <a>Last Name</a>
-            </div>
-            <div className="header__item">
-              <a>Country</a>
-            </div>
-            <div className="header__item">
-              <a>Address</a>
-            </div>
-            <div className="header__item">
-              <a>City</a>
-            </div>
-            <div className="header__item">
-              <a>State</a>
-            </div>
-            <div className="header__item">
-              <a>Zip</a>
-            </div>
-            <div className="header__item">
-              <a>Phone</a>
-            </div>
-          </div>
-          <div className="table-content">
-            <div className="table-row">
-              {users.map(user => (
-                <ul className="table-data" key={user.login.username}>
-                  <li>{user.name.first}</li>
-                </ul>
-              ))}
-            </div>
-            <div className="table-row">
-              {users.map(user => (
-                <ul className="table-data" key={user.login.username}>
-                  <li>{user.name.last}</li>
-                </ul>
-              ))}
-            </div>
-            <div className="table-row">
-              {users.map(user => (
-                <ul className="table-data" key={user.login.username}>
-                  <li>{user.nat}</li>
-                </ul>
-              ))}
-            </div>
-            <div className="table-row">
-              {users.map(user => (
-                <ul className="table-data" key={user.login.username}>
-                  <li>{user.location.street}</li>
-                </ul>
-              ))}
-            </div>
-            <div className="table-row">
-              {users.map(user => (
-                <ul className="table-data" key={user.login.username}>
-                  <li>{user.location.city}</li>
-                </ul>
-              ))}
-            </div>
-            <div className="table-row">
-              {users.map(user => (
-                <ul className="table-data" key={user.login.username}>
-                  <li>{user.location.state}</li>
-                </ul>
-              ))}
-            </div>
-            <div className="table-row">
-              {users.map(user => (
-                <ul className="table-data" key={user.login.username}>
-                  <li>{user.location.postcode}</li>
-                </ul>
-              ))}
-            </div>
-            <div className="table-row">
-              {users.map(user => (
-                <ul className="table-data" key={user.login.username}>
-                  <li>{user.cell}</li>
-                </ul>
-              ))}
+        <Loader />
+        {this.state.isLoading ? (
+          <Loader />
+        ) : (
+          <div className="table-container">
+            <div className="responsive-table">
+              <li id="table-head" className="table-header">
+                <div className="col col-1">First name</div>
+                <div className="col col-2">Last Name</div>
+                <div className="col col-3">Country</div>
+                <div className="col col-4">Address</div>
+                <div className="col col-5">City</div>
+                <div className="col col-5">State</div>
+                <div className="col col-7">Zip</div>
+                <div className="col col-8">Phone</div>
+              </li>
+              <ul class="responsive-table">
+                {users.map(user => (
+                  <li class="table-row">
+                    <div
+                      className="col col-1"
+                      data-label="First Name"
+                      key={user.login.username}
+                    >
+                      {user.name.first}
+                    </div>
+                    <div
+                      className="col col-2"
+                      data-label="Last Name"
+                      key={user.login.username}
+                    >
+                      {user.name.last}
+                    </div>
+                    <div
+                      className="col col-3"
+                      data-label="Country"
+                      key={user.login.username}
+                    >
+                      {user.nat}
+                    </div>
+                    <div
+                      className="col col-4"
+                      data-label="Address"
+                      key={user.login.username}
+                    >
+                      {user.location.street}
+                    </div>
+                    <div
+                      className="col col-5"
+                      data-label="Address"
+                      key={user.login.username}
+                    >
+                      {user.location.city}
+                    </div>
+                    <div
+                      className="col col-6"
+                      data-label="State"
+                      key={user.login.username}
+                    >
+                      {user.location.state}
+                    </div>
+                    <div
+                      className="col col-7"
+                      data-label="Zip"
+                      key={user.login.username}
+                    >
+                      {user.location.postcode}
+                    </div>
+
+                    <div
+                      className="col col-8"
+                      data-label="Phone"
+                      key={user.login.username}
+                    >
+                      {user.cell}
+                    </div>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
-        </div>
+        )}
       </div>
     );
   }
